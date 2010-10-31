c9ab1a6b2769351a8feb781b513d68b50066db92
yabeken <yabeken@gmail.com>
2010-10-31 22:37:39 +0900

    refactored codes

--- src/org/velzno/codeassist/FunctionCompletionContext.java
+++ src/org/velzno/codeassist/FunctionCompletionContext.java
@@ -19,17 +19,20 @@ import org.eclipse.php.internal.core.codeassist.contexts.ClassMemberContext;
 public class FunctionCompletionContext extends ClassMemberContext{
 	private static String className = "";
 	private static String functionName = "";
+	private static Pattern p1 = Pattern.compile("(R|C)\\s*\\(\\s*((?:[\"\'])?)(?:[a-zA-Z][a-zA-Z0-9-_]*\\.)*([a-zA-Z][a-zA-Z0-9_]*?)\\s*\\2\\)\\s*$");
+	private static Pattern p2 = Pattern.compile("(R)\\s*\\(\\s*new\\s+([a-zA-Z][a-zA-Z0-9_]*?)(?:\\s*\\(.*?\\))?\\s*\\)\\s*$");
+	private static Pattern p3 = Pattern.compile("(R|C)\\s*\\(\\s*(\\$[a-zA-Z][a-zA-Z0-9_]*?)\\s*\\)\\s*$");
 	public boolean isValid(ISourceModule sourceModule, int offset, CompletionRequestor requestor) {
-		clearNames();
 		try {
+			clearNames();
 			String source = sourceModule.getSource().substring(0, offset);
 			source = source.substring(0, source.lastIndexOf("->"));
 			//R(Hoge) R("org.rhaco.Hoge")
-			Matcher m1 = Pattern.compile("(R|C)\\s*\\(\\s*((?:[\"\'])?)(?:[a-zA-Z][a-zA-Z0-9-_]*\\.)*([a-zA-Z][a-zA-Z0-9_]*?)\\s*\\2\\)\\s*$").matcher(source);
+			Matcher m1 = p1.matcher(source);
 			//R(new Hoge($abc)) R(new Hoge("abc"))
-			Matcher m2 = Pattern.compile("(R)\\s*\\(\\s*new\\s+([a-zA-Z][a-zA-Z0-9_]*?)(?:\\s*\\(.*?\\))?\\s*\\)\\s*$").matcher(source);
+			Matcher m2 = p2.matcher(source);
 			//R($hoge)
-			Matcher m3 = Pattern.compile("(R|C)\\s*\\(\\s*(\\$[a-zA-Z][a-zA-Z0-9_]*?)\\s*\\)\\s*$").matcher(source);
+			Matcher m3 = p3.matcher(source);
 			if(m1.find()){
 				className = m1.group(3);
 				functionName = m1.group(1);
@@ -37,22 +40,17 @@ public class FunctionCompletionContext extends ClassMemberContext{
 				className = m2.group(2);
 				functionName = m2.group(1);
 			}else if(m3.find()){
-				//TODO
 				functionName = m3.group(1);
 				IType type = CodeAssistUtils.getVariableType(sourceModule, m3.group(2), getOffset())[0];
 				className = type.getElementName().toString();
+			}else{
+				return false;
 			}
-		} catch (Exception e) {
-			clearNames();
-			return false;
-		}
-		try{
 			if(super.isValid(sourceModule, offset, requestor) && getTriggerType() == Trigger.OBJECT){
 				return true;
 			}
-		}catch(Exception e){
-			
-			return false;
+		} catch (Exception e) {
+			clearNames();
 		}
 		return false;
 	}
